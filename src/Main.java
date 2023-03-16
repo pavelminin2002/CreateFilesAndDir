7f8458b1f057006e5758116363b3233bc65e0b6d
Pavel Minin <minin09092002@mail.ru>
2023-03-16 22:49:49 +0300

    for Task2

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,9 @@
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 public class Main {
     public static void main(String[] args) {
@@ -15,10 +16,38 @@ public class Main {
         makeDirs(namesDir, 4, 6, path, sb);
         path = path + "//main";
         makeFiles(nameFiles, 0, 2, path, sb);
-        path = path.replaceAll("//src//main", "//res");
+        path = path.replaceAll("src//main", "res");
         makeDirs(namesDir, 6, namesDir.size(), path, sb);
-        path = path.replaceAll("//res", "//temp");
+        path = path.replaceAll("res", "temp");
         makeFiles(nameFiles, 2, nameFiles.size(), path, sb);
+
+        path = path.replaceAll("temp", "savegames");
+        List<File> files = new ArrayList<>();
+        for (int i = 0; i < 3; i++) {
+            int health = (int) (Math.random() * 101);
+            int weapons = (int) (Math.random() * 101);
+            int lvl = (int) (Math.random() * 101);
+            double distance = Math.random() * 1001;
+
+            File file = new File(path, "SaveFile" + (i + 1) + ".dat");
+            files.add(file);
+            try {
+                if (file.createNewFile()) {
+                    sb.append("Файл " + file.getName() + " для сохранения успешно создан\n");
+                } else {
+                    sb.append("Файл " + file.getName() + " для сохранения не создан\n");
+                }
+            } catch (IOException e) {
+                sb.append("Файл " + file.getName() + " для сохранения не доступен\n");
+                e.printStackTrace();
+            }
+            saveGame(new GameProgress(health, weapons, lvl, distance), file, sb);
+        }
+
+        zipFiles(path, files, sb);
+        files.forEach(File::delete);
+
+        path = path.replaceAll("savegames", "temp");
         try (FileWriter fw = new FileWriter(new File(path, "temp.txt"))) {
             fw.write(sb.toString());
             fw.write("Файл temp.txt успешно перезаписан");
@@ -64,6 +93,39 @@ public class Main {
             } else {
                 sb.append("Директория " + namesDir.get(i) + " не создана \n");
             }
+        }
+    }
+
+    public static void saveGame(GameProgress gameProgress, File file, StringBuilder sb) {
+
+        try (FileOutputStream fos = new FileOutputStream(file);
+             ObjectOutputStream oos = new ObjectOutputStream(fos)) {
+            oos.writeObject(gameProgress);
+            oos.flush();
+            sb.append("Файл " + file.getName() + " для сохранения удалось перезаписать\n");
+        } catch (IOException e) {
+            sb.append("Файл " + file.getName() + " для сохранения не удалось перезаписать\n");
+            e.printStackTrace();
+        }
+    }
+
+    public static void zipFiles(String path, List<File> files, StringBuilder sb) {
+        try (ZipOutputStream zos = new ZipOutputStream(new FileOutputStream(path + "//" + "ZipSave.zip"))) {
+            for (File file : files) {
+                ZipEntry ze = new ZipEntry(file.getName());
+                zos.putNextEntry(ze);
+                try (FileInputStream in = new FileInputStream(file)) {
+                    byte[] b = new byte[in.available()];
+                    in.read(b);
+
+                    zos.write(b);
+                }
+                zos.closeEntry();
+                sb.append("Файл " + file.getName() + " был записан в архив\n");
+            }
+        } catch (IOException e) {
+            sb.append("Ошибка при записи файлов\n");
+            e.printStackTrace();
         }
     }
 
